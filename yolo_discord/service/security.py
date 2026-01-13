176c68303e38aa1e37eda3f9541ccd38e7627f70
Raas Ahsan <2617549+RaasAhsan@users.noreply.github.com>
2026-01-13 02:52:29 -0600

    Clean caching

--- yolo_discord/service/security.py
+++ yolo_discord/service/security.py
@@ -1,6 +1,6 @@
 import abc
 import aiohttp
-import cachebox
+from cachebox import TTLCache
 from moneyed import Money
 from typing import Optional
 
@@ -15,36 +15,49 @@ class SecurityService(abc.ABC):
 
 class SecurityServiceImpl(SecurityService):
     finnhub_api_key: str
+    price_cache: TTLCache
 
     def __init__(self, finnhub_api_key: str) -> None:
         self.finnhub_api_key = finnhub_api_key
+        self.price_cache = TTLCache(0, ttl=900)
 
     async def get_security_price(self, name: str) -> Optional[Money]:
-        return await get_security_price(self.finnhub_api_key, name)
+        async with aiohttp.ClientSession() as session:
+            return await self.fetch_price_through_cache(session, name)
 
     async def get_security_prices(self, names: list[str]) -> Optional[dict[str, Money]]:
         prices: dict[str, Money] = {}
-        for name in names:
-            price = await get_security_price(self.finnhub_api_key, name)
-            if price is None:
-                return None
-            prices[name] = price
+        async with aiohttp.ClientSession() as session:
+            for name in names:
+                price = await self.fetch_price_through_cache(session, name)
+                if price is None:
+                    return None
+                prices[name] = price
         return prices
+    
+    async def fetch_price_through_cache(self, session: aiohttp.ClientSession, name: str) -> Optional[Money]:
+        cache_hit = self.price_cache.get(name)
+        if cache_hit is not None:
+            return cache_hit
+        price = await fetch_security_price(session, self.finnhub_api_key, name)
+        self.price_cache[name] = price
+        return price
 
 
-@cachebox.cached(cachebox.TTLCache(0, ttl=900))
-async def get_security_price(token: str, symbol: str) -> Optional[Money]:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(
-            f"https://finnhub.io/api/v1/quote?symbol={symbol}&token={token}"
-        ) as resp:
-            if resp.status == 404:
-                return None
-            elif resp.status != 200:
-                text = await resp.text()
-                raise Exception(
-                    f"API call to finnhub failed with status {resp.status} and response: {text}"
-                )
-            response = await resp.json()
-            price = response["c"]
-            return Money(price, "USD")
+
+async def fetch_security_price(
+    session: aiohttp.ClientSession, token: str, symbol: str
+) -> Optional[Money]:
+    async with session.get(
+        f"https://finnhub.io/api/v1/quote?symbol={symbol}&token={token}"
+    ) as resp:
+        if resp.status == 404:
+            return None
+        elif resp.status != 200:
+            text = resp.text()
+            raise Exception(
+                f"API call to finnhub failed with status {resp.status} and response: {text}"
+            )
+        response = await resp.json()
+        price = response["c"]
+        return Money(price, "USD")
