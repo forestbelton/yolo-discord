47d241b980a722cdafd4b5f8673b0197bf52edd3
Raas Ahsan <2617549+RaasAhsan@users.noreply.github.com>
2026-01-13 00:10:20 -0600

    Add cache for pricing

--- yolo_discord/service/security.py
+++ yolo_discord/service/security.py
@@ -1,5 +1,6 @@
 import abc
 import aiohttp
+import cachebox
 from moneyed import Money
 from typing import Optional
 
@@ -19,33 +20,32 @@ class SecurityServiceImpl(SecurityService):
         self.finnhub_api_key = finnhub_api_key
 
     async def get_security_price(self, name: str) -> Optional[Money]:
-        async with aiohttp.ClientSession() as session:
-            return await get_security_price(session, self.finnhub_api_key, name)
+        return await get_security_price(self.finnhub_api_key, name)
 
     async def get_security_prices(self, names: list[str]) -> Optional[dict[str, Money]]:
         prices: dict[str, Money] = {}
-        async with aiohttp.ClientSession() as session:
-            for name in names:
-                price = await get_security_price(session, self.finnhub_api_key, name)
-                if price is None:
-                    return None
-                prices[name] = price
+        for name in names:
+            price = await get_security_price(self.finnhub_api_key, name)
+            if price is None:
+                return None
         return prices
 
 
+@cachebox.cached(cachebox.TTLCache(0, ttl=900))
 async def get_security_price(
-    session: aiohttp.ClientSession, token: str, symbol: str
+    token: str, symbol: str
 ) -> Optional[Money]:
-    async with session.get(
-        f"https://finnhub.io/api/v1/quote?symbol={symbol}&token={token}"
-    ) as resp:
-        if resp.status == 404:
-            return None
-        elif resp.status != 200:
-            text = resp.text()
-            raise Exception(
-                f"API call to finnhub failed with status {resp.status} and response: {text}"
-            )
-        response = await resp.json()
-        price = response["c"]
-        return Money(price, "USD")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            f"https://finnhub.io/api/v1/quote?symbol={symbol}&token={token}"
+        ) as resp:
+            if resp.status == 404:
+                return None
+            elif resp.status != 200:
+                text = await resp.text()
+                raise Exception(
+                    f"API call to finnhub failed with status {resp.status} and response: {text}"
+                )
+            response = await resp.json()
+            price = response["c"]
+            return Money(price, "USD")
